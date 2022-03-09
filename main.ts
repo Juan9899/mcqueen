496f623d3968caf60605e4bab28baa39747a4528
Juan9899 <98743364+Juan9899@users.noreply.github.com>
2022-03-09 09:28:44 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
+function derecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 173)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 173)
+    basic.pause(250)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function izquierda () {
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 173)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 173)
+    basic.pause(250)
+    maqueen.motorStop(maqueen.Motors.All)
+}
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 173)
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 8) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 9) {
         maqueen.motorStop(maqueen.Motors.All)
-        basic.pause(200)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 173)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 173)
-        basic.pause(500)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 173)
-        basic.pause(600)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 173)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 173)
-        basic.pause(200)
+        izquierda()
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 154)
+        basic.pause(1000)
+        derecha()
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 173)
     }
